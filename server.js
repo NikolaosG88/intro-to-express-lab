0dc2c66c5c97ad18b03f1e4fd02dc57c9290fbf6
NikolaosG <Nikolaos_Gkazgkas@outlook.com>
2024-09-25 00:23:28 +0100

    upated but 4 is short

--- server.js
+++ server.js
@@ -53,11 +53,28 @@ app.get('/collectibles/:index', (req, res) => {
 
   console.log('________4. Filter Shoes by Query Parameters!_______________________');
 
+  const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
 
 
+  app.get('/shoes/', (req, res) => {
+       
+    productList ='';
+    shoes.forEach((product) => {
+      productList += `<br> Brand: ${product.name}: Value: ${product.price} £, Type: ${product.type}<br>`;
+    })
+    res.send(productList);
+     
+});
 
 
-  
 
 // ____________________listeners____________________________//
 
